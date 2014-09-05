5d1c030dc636b303c6f9719d15fabb65375b606a
Jianru Lin <jianru.lin@gmail.com>
2014-09-06 00:31:22 +0800

    用链式操作重新描述了全部的模板

--- root/js/main.js
+++ root/js/main.js
@@ -1,12 +1,21 @@
 function demo() {
-	do {
-		console.log('hello')
-	} while (a > 100)
+	var o = {
+		name: 'Jianru',
+		email: 'jianru.lin@gmail.com',
+		show: function() {}
+	}
+
+	console.log(o.name, 100, f(1))
+
+	var arr = [{},{},3]
+	var arr2 = [1+2]
+	var arr3 = [new Object('hello'), new Object(), new Object]
+
 }
 
 $(function() {
 	var text = demo.toString()
-	text = text.substring(17, text.length-2)
+	text = text.substring(17, text.length - 2)
 	var ast = esprima.parse(text)
 	console.log(ast)
 	var dom = ast_to_dom(ast)
@@ -15,48 +24,48 @@ $(function() {
 
 var handler_map = {
 	'Program': function(ast) {
-		return div2(ast.type).append_ast(ast.body).dom()
+		return div(ast.type).append_ast(ast.body).dom()
 	},
 	'VariableDeclaration': function(ast) {
 		return (
-			div2(ast.type)
-				.append(span2('keyword', 'pre').text('var '))
-				.append(span2('box').append_ast(ast.declarations))
+			div(ast.type)
+				.append(span('keyword', 'pre').text('var '))
+				.append(span('box').append_ast(ast.declarations))
 				.dom()
 		)
 	},
 	'VariableDeclarator': function(ast) {
 		if (!ast.init) {
 			return (
-				div2(ast.type).append_ast(ast.id)
+				div(ast.type).append_ast(ast.id).dom()
 			)
 		}
 		else {
 			return (
-				div2(ast.type)
-					.append(span2('id').append_ast(ast.id))
-					.append(span2('equ').text(' = '))
-					.append(span2('init').append_ast(ast.init))
+				div(ast.type)
+					.append(span('id').append_ast(ast.id))
+					.append(span('equ').text(' = '))
+					.append(span('init').append_ast(ast.init))
 					.dom()
 			)
 		}
 	},
 	'Identifier': function(ast) {
-		return span2(ast.type).text(ast.name).dom()
+		return span(ast.type).text(ast.name).dom()
 	},
 	'Literal': function(ast) {
-		return span2(ast.type).text(ast.raw).dom()
+		return span(ast.type).text(ast.raw).dom()
 	},
 	'IfStatement': function(ast) {
 		var d = 
-			div2(ast.type)
+			div(ast.type)
 				.append(
-					span2('keyword', 'pre').text('if '))
+					span('keyword', 'pre').text('if '))
 				.append(
-					span2('test')
+					span('test')
 						.append_ast(ast.test))
 				.append(
-					div2('indent')
+					div('indent')
 						.append_ast(ast.consequent))
 
 		// else if ...
@@ -70,12 +79,12 @@ var handler_map = {
 			else if (alternate.type === 'IfStatement') {
 				d
 					.append(
-						span2('keyword', 'pre').text('else if '))
+						span('keyword', 'pre').text('else if '))
 					.append(
-						span2('test')
+						span('test')
 							.append_ast(alternate.test))
 					.append(
-						div2('indent')
+						div('indent')
 							.append_ast(alternate.consequent))
 
 					// 继续递归生成 else if ...
@@ -84,9 +93,9 @@ var handler_map = {
 			else {
 				d
 					.append(
-						span2('keyword').text('else'))
+						span('keyword').text('else'))
 					.append(
-						div2('indent')
+						div('indent')
 							.append_ast(alternate))
 
 				return d.dom()
@@ -94,13 +103,13 @@ var handler_map = {
 		}
 	},
 	'BlockStatement': function(ast) {
-		return div2(ast.type).append_ast(ast.body).dom()
+		return div(ast.type).append_ast(ast.body).dom()
 	},
 	'BinaryExpression': function(ast) {
 		return (
-			div2(ast.type)
+			span(ast.type)
 				.append_ast(ast.left)
-				.append(span2('operator', 'pre').text(' ' + ast.operator + ' '))
+				.append(span('operator', 'pre').text(' ' + ast.operator + ' '))
 				.append_ast(ast.right)
 				.dom()
 		)
@@ -109,177 +118,239 @@ var handler_map = {
 
 		if (ast.prefix) {
 			return (
-				div2(ast.type)
-					.append(span2('operator', 'pre').text(' ' + ast.operator))
+				span(ast.type)
+					.append(span('operator', 'pre').text(ast.operator + ' '))
 					.append_ast(ast.argument)
 					.dom()
 			)
 		}
 		else {
 			return (
-				div2(ast.type)
+				span(ast.type)
 					.append_ast(ast.argument)
-					.append(span2('operator', 'pre').text(' ' + ast.operator))
+					.append(span('operator', 'pre').text(' ' + ast.operator))
 					.dom()
 			)
 		}
 	},
 	'UnaryExpression': function(ast) {
-		var d = _class(div(), ast.type)
 		if (ast.prefix) {
-			append(d, text(_class(div(), 'operator'), ' ' + ast.operator + ' '))
-			append(d, ast_to_dom(ast.argument))
-		}
-		else {
-			append(d, ast_to_dom(ast.argument))
-			append(d, text(_class(div(), 'operator'), ' ' + ast.operator + ' '))
-		}
-		return d
-	},
-	'LogicalExpression': function(ast) {
-		var d = _class(div(), ast.type)
-		append(d, ast_to_dom(ast.left))
-		append(d, text(_class(div(), 'operator'), ' ' + ast.operator + ' '))
-		append(d, ast_to_dom(ast.right))
-		return d
-	},
-	'ConditionalExpression': function(ast) {
-		var d = _class(div(), ast.type)
-		append(d, ast_to_dom(ast.test))
-		append(d, text(_class(div(), 'operator'), ' ? '))
-		append(d, ast_to_dom(ast.consequent))
-		append(d, text(_class(div(), 'operator'), ' : '))
-		append(d, ast_to_dom(ast.alternate))
-		return d
-	},
-	'AssignmentExpression': function(ast) {
-		var d = _class(div(), ast.type)
-		append(d, ast_to_dom(ast.left))
-		append(d, text(_class(div(), 'operator'), ' ' + ast.operator + ' '))
-		append(d, ast_to_dom(ast.right))
-		return d
-	},
-	'ExpressionStatement': function(ast) {
-		var d = _class(div(), ast.type)
-		append(d, ast_to_dom(ast.expression))
-		return d
-	},
-	'CallExpression': function(ast) {
-		var d = _class(div(), ast.type)
-		append(d, ast_to_dom(ast.callee))
-		append(d, text(_class(div(), ['bracket', 'left']), '('))
-		// arguments
-		if (ast.arguments && ast.arguments.length > 0) {
-			ast.arguments.forEach(function(arg, i) {
-				append(d, ast_to_dom(arg))
-				if (i < ast.arguments.length - 1) {
-					append(d, text(_class(div(), 'comma'), ', '))
-				}
-			});
-		}
-		append(d, text(_class(div(), ['bracket', 'right']), ')'))
-		return d
-	},
-	'ObjectExpression': function(ast) {
-		var d = _class(div(), ast.type)
-		append(d, text(_class(div(), ['bracket', 'left']), '{'))
-		// properties
-		if (ast.properties && ast.properties.length > 0) {
-			ast.properties.forEach(function(p, i) {
-				var _ = _class(div(), p.type)
-				append(_, ast_to_dom(p.key))
-				append(_, text(_class(div(), 'colon'), ': '))
-				append(_, ast_to_dom(p.value))
-				append(d, _)
-				if (i < ast.properties.length - 1) {
-					append(d, text(_class(div(), 'comma'), ', '))
-				}
-			})
-		}
-		append(d, text(_class(div(), ['bracket', 'right']), '}'))
-		return d
-	},
-	'ArrayExpression': function(ast) {
-		var d = _class(div(), ast.type)
-		append(d, text(_class(div(), ['bracket', 'left']), '['))
-		// elements
-		if (ast.elements && ast.elements.length > 0) {
-			ast.elements.forEach(function(e, i) {
-				// e can be null
-				if (e) {
-					append(d, ast_to_dom(e))
-					if (i < ast.elements.length - 1) {
-						append(d, text(_class(div(), 'comma'), ', '))
-					}
-				}
-				else {
-					append(d, text(_class(div(), 'comma'), ', '))
-				}
-			})
-		}
-		append(d, text(_class(div(), ['bracket', 'right']), ']'))
-		return d
-	},
-	'NewExpression': function(ast) {
-		var d = _class(div(), ast.type)
-		append(d, text(_class(div(), 'new'), 'new '))
-		append(d, ast_to_dom(ast.callee))
-		// arguments
-		append(d, text(_class(div(), ['bracket', 'left']), '('))
-		if (ast.arguments && ast.arguments.length > 0) {
-			ast.arguments.forEach(function(e, i) {
-				append(d, ast_to_dom(e))
-				if (i < ast.arguments.length - 1) {
-					append(d, text(_class(div(), 'comma'), ', '))
-				}
-			})
-		}
-		append(d, text(_class(div(), ['bracket', 'right']), ')'))
-		return d
-	},
-	'MemberExpression': function(ast) {
-		var prop = ast.property
-		if (prop.type === 'Literal' && /^['"]/.test(prop.raw)) {
 			return (
-				div2(ast.type)
-					.append_ast(ast.object)
-					.append(span2().text('['))
-					.append_ast(prop)
-					.append(span2().text(']'))
+				span(ast.type)
+					.append(span('operator', 'pre').text(ast.operator + ' '))
+					.append_ast(ast.argument)
 					.dom()
 			)
 		}
 		else {
 			return (
-				div2(ast.type)
+				span(ast.type)
+					.append_ast(ast.argument)
+					.append(span('operator', 'pre').text(' ' + ast.operator))
+					.dom()
+			)
+		}
+	},
+	'LogicalExpression': function(ast) {
+		return (
+			span(ast.type)
+				.append_ast(ast.left)
+				.append(span('operator', 'pre').text(' ' + ast.operator + ' '))
+				.append_ast(ast.right)
+				.dom()
+		)
+	},
+	'ConditionalExpression': function(ast) {
+		return (
+			span(ast.type)
+				.append(span().text('('))
+				.append_ast(ast.test)
+				.append(span('operator', 'pre').text(' ? '))
+				.append_ast(ast.consequent)
+				.append(span('operator', 'pre').text(' : '))
+				.append_ast(ast.alternate)
+				.append(span().text(')'))
+				.dom()
+		)
+	},
+	'AssignmentExpression': function(ast) {
+		return (
+			span(ast.type)
+				.append_ast(ast.left)
+				.append(span('operator', 'pre').text(' ' + ast.operator + ' '))
+				.append_ast(ast.right)
+				.dom()
+		)
+	},
+	'ExpressionStatement': function(ast) {
+		return div(ast.type).append_ast(ast.expression).dom()
+	},
+	'CallExpression': function(ast) {
+		var args = ast.arguments
+		if (args && args.length > 0) {
+
+			var t =
+				span(ast.type)
+					.append_ast(ast.callee)
+					.append(span('pre').text('( '))
+		
+			ast.arguments.forEach(function(arg, i) {
+				t.append_ast(arg)
+				if (i < ast.arguments.length - 1) {
+					t.append(span('pre').text(', '))
+				}
+			})
+
+			t.append(span('pre').text(' )'))
+			return t.dom()
+		}
+		else {
+
+			return (
+				span(ast.type)
+					.append_ast(ast.callee)
+					.append(span().text('()'))
+					.dom()
+			)
+
+		}
+	},
+	'ObjectExpression': function(ast) {
+
+		var props = ast.properties
+
+		if (props && props.length > 0) {
+
+			var t =
+				span(ast.type)
+					.append(span('pre').text('{ '))
+
+			props.forEach(function(prop, i) {
+				t
+					.append(
+						span(prop.type)
+							.append_ast(prop.key)
+							.append(span('pre').text(': '))
+							.append_ast(prop.value))
+
+				if (i < props.length - 1) {
+					t.append(span('pre').text(', '))
+				}
+			})
+
+			t.append(span('pre').text(' }'))
+			return t.dom()
+		}
+		else {
+			return (
+				span(ast.type)
+					.append(span('pre').text('{}'))
+					.dom()
+			)
+		}
+
+	},
+	'ArrayExpression': function(ast) {
+		var elements = ast.elements
+		if (elements && elements.length > 0) {
+			var t = 
+				span(ast.type)
+					.append(span('pre').text('[ '))
+
+			elements.forEach(function(e, i) {
+				t.append_ast(e)
+				if (i < elements.length - 1) {
+					t.append(span('pre').text(', '))
+				}
+			})
+
+			t.append(span('pre').text(' ]'))
+			return t.dom()
+		}
+		else {
+			return (
+				span(ast.type)
+					.append(span('pre').text('[]'))
+					.dom()
+			)
+		}
+	},
+	'NewExpression': function(ast) {
+		var args = ast.arguments
+		if (args && args.length > 0) {
+
+			var t =
+				span(ast.type)
+					.append(span('pre').text('new '))
+					.append_ast(ast.callee)
+					.append(span().text('('))
+
+			args.forEach(function(arg, i) {
+				t.append_ast(arg)
+				if (i < args.length - 1) {
+					t.append(span().text(', '))
+				}
+			})
+
+			t.append(span().text(')'))
+			return t.dom()
+
+		}
+		else {
+
+			return (
+				span(ast.type)
+					.append(span('pre').text('new '))
+					.append_ast(ast.callee)
+					.append(span().text('()'))
+					.dom()
+			)
+
+		}
+	},
+	'MemberExpression': function(ast) {
+		var prop = ast.property
+		if (prop.type === 'Literal' && /^['"]/.test(prop.raw)) {
+			return (
+				span(ast.type)
 					.append_ast(ast.object)
-					.append(span2().text('.'))
+					.append(span().text('['))
+					.append_ast(prop)
+					.append(span().text(']'))
+					.dom()
+			)
+		}
+		else {
+			return (
+				span(ast.type)
+					.append_ast(ast.object)
+					.append(span().text('.'))
 					.append_ast(prop)
 					.dom()
 			)
 		}
 	},
 	'FunctionExpression': function(ast) {
-		return div2(ast.type).text('{λ}').dom()
+		return span(ast.type).text('{λ}').dom()
 	},
 	'FunctionDeclaration': function(ast) {
 		return (
-			div2(ast.type)
-				.append(span2('keyword', 'pre').text('function '))
+			div(ast.type)
+				.append(span('keyword', 'pre').text('function '))
 				.append_ast(ast.id)
-				.append(span2().text('() {}'))
+				.append(span().text('() {}'))
 				.dom()
 		)
 	},
 	'DoWhileStatement': function(ast) {
 		return (
-			div2(ast.type)
-				.append(span2('keyword').text('do'))
+			div(ast.type)
+				.append(span('keyword').text('do'))
 				.append(
-					div2('indent')
+					div('indent')
 						.append_ast(ast.body))
 				.append(
-					span2('keyword', 'pre').text('while '))
+					span('keyword', 'pre').text('while '))
 				.append_ast(ast.test)
 		)
 	}
@@ -304,34 +375,6 @@ function ast_list_to_dom(ast_list) {
 }
 
 function div() {
-	return document.createElement('div')
-}
-
-function _class(e, list) {
-	if (!e) return
-	if (!Array.isArray(list)) list = [list]
-	list.forEach(function(c) {
-		if (c) e.classList.add(c)
-	})
-	return e
-}
-
-function text(e, v) {
-	if (!e) return e
-	e.textContent = v
-	return e
-}
-
-function append(e, list) {
-	if (!e) return
-	if (!Array.isArray(list)) list = [list]
-	list.forEach(function(item) {
-		if (item) e.appendChild(item)
-	})
-	return e
-}
-
-function div2() {
 	var class_ = []
 	for (var i = 0; i < arguments.length; ++i) {
 		class_.push(arguments[i])
@@ -339,7 +382,7 @@ function div2() {
 	return new E('div', class_)
 }
 
-function span2() {
+function span() {
 	var class_ = []
 	for (var i = 0; i < arguments.length; ++i) {
 		class_.push(arguments[i])
