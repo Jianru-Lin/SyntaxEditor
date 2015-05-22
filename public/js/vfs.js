a23824afccb8e0706569fb44c73dda7adffdbe0f
Jianru Lin <jianru.lin@gmail.com>
2015-05-22 19:06:07 +0800

    vfs

---
+++ public/js/vfs.js
@@ -0,0 +1,47 @@
+function vfsFile(name, content) {
+
+}
+
+vfsFile.prototype.getName = function() {
+
+}
+
+vfsFile.prototype.setName = function() {
+
+}
+
+vfsFile.prototype.getContent = function() {
+
+}
+
+vfsFile.prototype.setContent = function() {
+
+}
+
+vfsFile.prototype.onRename = function(cb) {
+
+}
+
+vfsFile.prototype.onRemove = function(cb) {
+
+}
+
+vfsFile.prototype.onChangeContent = function(cb) {
+
+}
+
+window.vfs = {
+	
+	findFile: function(name) {
+
+	},
+	addFile: function(name, content) {
+
+	},
+	removeFile: function(name) {
+
+	},
+	renameFile: function(oldName, newName) {
+
+	}
+}
